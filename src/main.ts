44da0288182444205eec2f219f3c6e38685357aa
adadychanka <artem_dadychenko@mail.ru>
2024-02-25 15:31:50 +0500

    Added swagger setup
    Signed-off-by: adadychanka <artem_dadychenko@mail.ru>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from "@nestjs/core"
+import { SwaggerModule } from "@nestjs/swagger"
+import { ConfigService } from "@nestjs/config"
 import { AppModule } from "./app.module"
+import { generateSwaggerConfig, SWAGGER_API_URL } from "./config/swagger-config"
+import { EnvVariables } from "./config/env-configuration"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+
+  const configService = app.get<ConfigService<EnvVariables>>(ConfigService)
+  const appPort = configService.get<number>("app_port")
+
+  const swaggerConfig = generateSwaggerConfig()
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup(SWAGGER_API_URL, app, swaggerDocument)
+
+  await app.listen(appPort)
 }
+
 bootstrap()
