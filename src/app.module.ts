90c84fccb494f0ef13e6b652add5840ba19114e6
adadychanka <artem_dadychenko@mail.ru>
2024-02-25 15:49:43 +0500

    Added simple health check
    Signed-off-by: adadychanka <artem_dadychenko@mail.ru>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from "@nestjs/common"
+import { ConfigModule } from "@nestjs/config"
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
-import { ConfigModule } from "@nestjs/config"
 import { loadEnvConfig } from "./config/env-configuration"
+import { HealthCheckModule } from "./health-check/health-check.module"
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [loadEnvConfig],
     }),
+    HealthCheckModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/health-check/health-check.module.ts
@@ -0,0 +1,10 @@
+import { HttpModule } from "@nestjs/axios"
+import { Module } from "@nestjs/common"
+import { TerminusModule } from "@nestjs/terminus"
+import { HealthCheckController } from "./health-check.controller"
+
+@Module({
+  imports: [TerminusModule, HttpModule],
+  controllers: [HealthCheckController],
+})
+export class HealthCheckModule {}
