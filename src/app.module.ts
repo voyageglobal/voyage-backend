89a88ae5923967fb7a460282749a74f888217975
adadychanka <artem_dadychenko@mail.ru>
2024-02-25 14:49:39 +0500

    Nest configuration setup
    Signed-off-by: adadychanka <artem_dadychenko@mail.ru>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common"
+import { AppController } from "./app.controller"
+import { AppService } from "./app.service"
+import { ConfigModule } from "@nestjs/config"
+import { loadEnvConfig } from "./config/env-configuration"
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [loadEnvConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/process-env.d.ts
@@ -0,0 +1,17 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: "development" | "production"
+      PORT: string
+
+      DB_USER: string
+      DB_PASSWORD: string
+      DB_NAME: string
+      DB_HOST: string
+      DB_PORT: string
+      DB_URL: string
+    }
+  }
+}
+
+export {}
