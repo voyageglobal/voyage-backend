70ce75d137c4c84f4632769399d24f7bfc628ff8
adadychanka <artem_dadychenko@mail.ru>
2024-02-25 15:49:43 +0500

    Added simple health check
    Signed-off-by: adadychanka <artem_dadychenko@mail.ru>

---
+++ src/health-check/health-check.controller.spec.ts
@@ -0,0 +1,21 @@
+import { HttpModule } from "@nestjs/axios"
+import { TerminusModule } from "@nestjs/terminus"
+import { Test, TestingModule } from "@nestjs/testing"
+import { HealthCheckController } from "./health-check.controller"
+
+describe("HealthCheckController", () => {
+  let controller: HealthCheckController
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [TerminusModule, HttpModule],
+      controllers: [HealthCheckController],
+    }).compile()
+
+    controller = module.get<HealthCheckController>(HealthCheckController)
+  })
+
+  it("should be defined", () => {
+    expect(controller).toBeDefined()
+  })
+})
