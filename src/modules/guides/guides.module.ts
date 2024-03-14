d4dc4030d41d03b3cc07acdb4aed7edcec950f4c
adadychanka <artem_dadychenko@mail.ru>
2024-03-14 15:31:37 +0500

    Implemented getting filtered guides
    Signed-off-by: adadychanka <artem_dadychenko@mail.ru>

--- src/modules/guides/guides.module.ts
+++ src/modules/guides/guides.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common"
+import { Logger, Module } from "@nestjs/common"
 import { PrismaModule } from "../prisma/prisma.module"
 import { GuidesService } from "./guides.service"
 import { GuidesController } from "./guides.controller"
@@ -6,7 +6,7 @@ import { GuidesController } from "./guides.controller"
 @Module({
   imports: [PrismaModule],
   controllers: [GuidesController],
-  providers: [GuidesService],
+  providers: [GuidesService, Logger],
   exports: [GuidesService],
 })
 export class GuidesModule {}
