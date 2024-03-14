001fe527dbb9a6c3bb40786f97b9b8c63d4d3c12
adadychanka <artem_dadychenko@mail.ru>
2024-03-14 22:58:52 +0500

    Excluded deleted prop from DTOs
    Signed-off-by: adadychanka <artem_dadychenko@mail.ru>

--- src/modules/guides/dto/guide.dto.ts
+++ src/modules/guides/dto/guide.dto.ts
@@ -1,4 +1,8 @@
+import { Exclude } from "class-transformer"
 import { OmitType, PartialType } from "@nestjs/swagger"
 import { Guide } from "../entities/guide.entity"
 
-export class GuideDto extends PartialType(OmitType(Guide, ["deleted"] as const)) {}
+export class GuideDto extends PartialType(OmitType(Guide, ["deleted"] as const)) {
+  @Exclude()
+  deleted: boolean
+}
