ac3791cc3d63b7d9e7665c50cc8a8d9c4e4e12e1
adadychanka <artem_dadychenko@mail.ru>
2024-03-14 21:40:51 +0500

    Improved get by id endpoint
    Signed-off-by: adadychanka <artem_dadychenko@mail.ru>

--- src/modules/guides/dto/guide.dto.ts
+++ src/modules/guides/dto/guide.dto.ts
@@ -1,16 +1,4 @@
-import { PartialType } from "@nestjs/swagger"
+import { OmitType, PartialType } from "@nestjs/swagger"
 import { Guide } from "../entities/guide.entity"
 
-export class GuideDto extends PartialType(Guide) {
-  id: string
-
-  name: string
-
-  text: string
-
-  deleted: boolean
-
-  createdAt: Date
-
-  updatedAt: Date
-}
+export class GuideDto extends PartialType(OmitType(Guide, ["deleted"] as const)) {}
