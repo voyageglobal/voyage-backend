d1a37782387ca5b9f3ae2d939e9badc2786b8079
adadychanka <a.dadychenko@gmail.com>
2024-03-28 23:25:57 +0500

    Added more mocks for unit-tests
    Signed-off-by: adadychanka <artem_dadychenko@mail.ru>
    Signed-off-by: adadychanka <a.dadychenko@gmail.com>

---
+++ src/test-utils/mocks/city.ts
@@ -0,0 +1,46 @@
+import { CityDto } from "../../modules/cities/dto/city.dto"
+import { City } from "../../modules/cities/entities/city.entity"
+import { getImageMock } from "./image"
+
+export function getCityMock(overrides: Partial<City> = {}): City {
+  return {
+    id: "city-1",
+    name: "Paris",
+    description: "The capital of France",
+    country: {
+      id: "country-1",
+      name: "France",
+      description: "A country in Europe",
+      cities: [],
+      images: [],
+      deleted: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    images: [getImageMock()],
+    deleted: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    ...overrides,
+  }
+}
+
+export function getCityDtoMock(overrides: Partial<CityDto> = {}): CityDto {
+  return {
+    id: "city-dto-1",
+    name: "Paris",
+    description: "The capital of France",
+    country: {
+      id: "country-1",
+      name: "France",
+      description: "A country in Europe",
+      cities: [],
+      images: [],
+      deleted: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    images: [],
+    ...overrides,
+  }
+}
