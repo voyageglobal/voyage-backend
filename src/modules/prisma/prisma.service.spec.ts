e56d369b70289f1246e33f82621660bd3203229d
adadychanka <a.dadychenko@gmail.com>
2024-04-02 21:19:25 +0500

    Covered guide creation with categories by unit-tests
    Signed-off-by: adadychanka <a.dadychenko@gmail.com>

--- src/modules/prisma/prisma.service.spec.ts
+++ src/modules/prisma/prisma.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from "@nestjs/testing"
+import { MockedLogger } from "../../test-utils/providers"
 import { PrismaService } from "./prisma.service"
 
 describe("PrismaService", () => {
@@ -6,7 +7,7 @@ describe("PrismaService", () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PrismaService],
+      providers: [PrismaService, MockedLogger],
     }).compile()
 
     service = module.get<PrismaService>(PrismaService)
@@ -14,5 +15,11 @@ describe("PrismaService", () => {
 
   it("should be defined", () => {
     expect(service).toBeDefined()
+  })
+
+  it("should call connect to db by triggering onModuleInit hook", async () => {
+    const connectSpy = jest.spyOn(service, "$connect")
+    await service.onModuleInit()
+    expect(connectSpy).toBeCalledTimes(1)
   })
 })
