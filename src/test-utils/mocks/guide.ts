c5ce43c4f096e45165dba345d97287cbc3643ea2
adadychanka <a.dadychenko@gmail.com>
2024-04-05 12:07:47 +0500

    Write unit-test for guide update operation
    Signed-off-by: adadychanka <a.dadychenko@gmail.com>

--- src/test-utils/mocks/guide.ts
+++ src/test-utils/mocks/guide.ts
@@ -1,5 +1,6 @@
 import { CreateGuideDto } from "../../modules/guides/dto/create-guide.dto"
 import { GuideDto } from "../../modules/guides/dto/guide.dto"
+import { UpdateGuideDto } from "../../modules/guides/dto/update-guide.dto"
 import { Guide } from "../../modules/guides/entities/guide.entity"
 
 export function getGuideMock(overrides: Partial<Guide> = {}): Guide {
@@ -39,6 +40,22 @@ export function getGuideDtoMock(overrides: Partial<GuideDto> = {}): GuideDto {
 
 export function getCreateGuideDtoMock(overrides: Partial<CreateGuideDto> = {}): CreateGuideDto {
   return {
+    name: "Test Guide",
+    text: "This is a test guide",
+    categories: [],
+    cities: [],
+    countries: [],
+    primaryImages: [],
+    contentImages: [],
+    visitedDateStart: new Date("2021-01-01T00:00:00Z"),
+    visitedDateEnd: new Date("2021-01-01T00:00:00Z"),
+    ...overrides,
+  }
+}
+
+export function getUpdateGuideDtoMock(overrides: Partial<UpdateGuideDto> = {}): UpdateGuideDto {
+  return {
+    id: "guide-1",
     name: "Test Guide",
     text: "This is a test guide",
     categories: [],
