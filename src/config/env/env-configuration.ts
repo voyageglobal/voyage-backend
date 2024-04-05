7de6bc4cf058038bc945f3bad40f4481f054aaaf
adadychanka <a.dadychenko@gmail.com>
2024-04-05 14:11:03 +0500

    Updated db config + extended docs
    Signed-off-by: adadychanka <a.dadychenko@gmail.com>

--- src/config/env/env-configuration.ts
+++ src/config/env/env-configuration.ts
@@ -25,7 +25,7 @@ export function getEnvironmentConfig(): EnvironmentConfig {
     app_port: parseInt(process.env.APP_PORT, 10) || DEFAULT_APP_PORT,
     db: {
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
+      port: parseInt(process.env.DB_PORT_OUT, 10),
       name: process.env.DB_NAME,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
