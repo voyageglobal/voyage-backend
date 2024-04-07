acc756100411d377c30395c53d002ed62c2bc8e9
adadychanka <a.dadychenko@gmail.com>
2024-04-08 00:41:58 +0500

    Added county input and output schemas
    Signed-off-by: adadychanka <a.dadychenko@gmail.com>

--- src/modules/prisma/seeds/guide-categories/utils.ts
+++ src/modules/prisma/seeds/guide-categories/utils.ts
@@ -1,7 +1,3 @@
 import { Prisma } from "@prisma/client"
-import { getSchemaValidator } from "../utils"
-import { schema } from "./schema"
-
-const validate = getSchemaValidator(schema)
 
 export type GuideCategoryCreateInput = Omit<Prisma.GuideCategoryCreateInput, "guides">
