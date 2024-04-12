cbe9c0a6132abcbcc0c6eec4ca31813f7f52ef4b
adadychanka <a.dadychenko@gmail.com>
2024-04-12 15:27:02 +0500

    Covered countries transformers by unit tests
    Signed-off-by: adadychanka <a.dadychenko@gmail.com>

--- src/modules/prisma/seeds/countries/transformers.ts
+++ src/modules/prisma/seeds/countries/transformers.ts
@@ -1,11 +1,13 @@
 import { CountryCreateInput, InputCountySchemaType } from "./schema"
 
 export function transformSeedInputToOutput(input: InputCountySchemaType[]): CountryCreateInput[] {
+  if (!input) return []
+
   return input.map(countryInput => {
     const dbCountry: CountryCreateInput = {
-      name: countryInput.name,
-      flag: countryInput.emoji,
-      description: countryInput.capital,
+      name: countryInput?.name || "",
+      flag: countryInput?.emoji || "",
+      description: countryInput?.capital || "",
     }
 
     return dbCountry
