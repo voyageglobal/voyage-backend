052a33ce1af6ab7fb2064bf5f8f5e68146f98a33
adadychanka <a.dadychenko@gmail.com>
2024-04-12 15:52:10 +0500

    Covered seed processor by unit tests
    Signed-off-by: adadychanka <a.dadychenko@gmail.com>

--- src/modules/prisma/seeds/utils/seed-processor.test.ts
+++ src/modules/prisma/seeds/utils/seed-processor.test.ts
@@ -1,6 +1,14 @@
+import { PrismaClient } from "@prisma/client"
+import { prismaMock } from "../../../../test-utils/prisma"
 import { createSeedProcessor } from "./seed-processor"
 
 describe("Seed processor", () => {
+  let prisma: PrismaClient
+
+  beforeEach(() => {
+    prisma = prismaMock as unknown as PrismaClient
+  })
+
   it("should return processor function", () => {
     const process = createSeedProcessor({
       validators: {
@@ -16,5 +24,78 @@ describe("Seed processor", () => {
     })
 
     expect(process).toBeInstanceOf(Function)
+  })
+
+  it("should throw an error if validate seed path function doesn't return true value", () => {
+    const process = createSeedProcessor({
+      validators: {
+        seedPathValidator: () => false,
+        seedOutputDataValidator: () => true,
+        seedInputDataValidator: () => true,
+      },
+      seedDataExtractor: () => [],
+      onProcessSeed: async () => {},
+      transformers: {
+        transformInput: data => data,
+      },
+    })
+
+    expect(() => process(prisma)).toThrow("Invalid seed path")
+  })
+
+  it("should throw an error if validate seed input data return false", () => {
+    const process = createSeedProcessor({
+      validators: {
+        seedPathValidator: () => true,
+        seedOutputDataValidator: () => true,
+        seedInputDataValidator: () => false,
+      },
+      seedDataExtractor: () => [],
+      onProcessSeed: async () => {},
+      transformers: {
+        transformInput: data => data,
+      },
+    })
+
+    expect(() => process(prisma)).toThrow("Invalid seed data")
+  })
+
+  it("should throw an error if validate seed output data return false", () => {
+    const process = createSeedProcessor({
+      validators: {
+        seedPathValidator: () => true,
+        seedOutputDataValidator: () => false,
+        seedInputDataValidator: () => true,
+      },
+      seedDataExtractor: () => [],
+      onProcessSeed: async () => {},
+      transformers: {
+        transformInput: data => data,
+      },
+    })
+
+    expect(() => process(prisma)).toThrow("Invalid transformed seed data")
+  })
+
+  it("should call return value from onProcessSeed callback", () => {
+    const processSeedResult = { count: 1 }
+    const onProcessSeed = jest.fn().mockResolvedValueOnce(processSeedResult)
+    const process = createSeedProcessor({
+      validators: {
+        seedPathValidator: () => true,
+        seedOutputDataValidator: () => true,
+        seedInputDataValidator: () => true,
+      },
+      seedDataExtractor: () => [],
+      onProcessSeed: onProcessSeed,
+      transformers: {
+        transformInput: data => data,
+      },
+    })
+
+    const promise = process(prisma)
+
+    expect(onProcessSeed).toHaveBeenCalled()
+    expect(promise).resolves.toEqual(processSeedResult)
   })
 })
