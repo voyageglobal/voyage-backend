b15402ea7ed3bed8df65ddaba63f19c58560532c
adadychanka <a.dadychenko@gmail.com>
2024-07-14 22:04:01 +0500

    Added stats service
    Signed-off-by: adadychanka <a.dadychenko@gmail.com>

--- src/modules/stats/stats.service.spec.ts
+++ src/modules/stats/stats.service.spec.ts
@@ -1,18 +1,46 @@
 import { Test, TestingModule } from "@nestjs/testing"
+import { PrismaClientMock, prismaMock } from "../../test-utils/prisma"
+import { MockedLogger } from "../../test-utils/providers"
+import { PrismaModule } from "../prisma/prisma.module"
+import { PrismaService } from "../prisma/prisma.service"
 import { StatsService } from "./stats.service"
 
 describe("StatsService", () => {
   let service: StatsService
+  let prisma: PrismaClientMock
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StatsService],
-    }).compile()
+      imports: [PrismaModule],
+      providers: [StatsService, PrismaService, MockedLogger],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(prismaMock)
+      .compile()
 
     service = module.get<StatsService>(StatsService)
+    prisma = prismaMock as unknown as PrismaClientMock
   })
 
   it("should be defined", () => {
     expect(service).toBeDefined()
+  })
+
+  describe("Get total stats", () => {
+    it("should return total stats", async () => {
+      prisma.$transaction.mockResolvedValue([10, 5, 20])
+
+      const totalStats = await service.getTotalStats()
+
+      expect(totalStats.totalCities).toEqual(10)
+      expect(totalStats.totalCountries).toEqual(5)
+      expect(totalStats.totalGuides).toEqual(20)
+    })
+
+    it("should throw an error if something goes wrong", async () => {
+      prisma.$transaction.mockRejectedValue(new Error("Test error"))
+
+      await expect(service.getTotalStats()).rejects.toThrow("Failed to get total stats")
+    })
   })
 })
--- src/modules/stats/stats.service.ts
+++ src/modules/stats/stats.service.ts
@@ -1,14 +1,58 @@
-import { Injectable } from "@nestjs/common"
+import { Injectable, Logger } from "@nestjs/common"
+import { PrismaService } from "../prisma/prisma.service"
 import { TotalStatsDto } from "./dto/total-stats.dto"
 
 @Injectable()
 export class StatsService {
+  constructor(
+    private readonly logger: Logger,
+    private readonly prismaService: PrismaService,
+  ) {}
+
   async getTotalStats(): Promise<TotalStatsDto> {
-    return {
-      totalUsers: 1000,
-      totalCities: 200,
-      totalGuides: 50,
-      totalCountries: 50,
+    try {
+      const [totalCities, totalCountries, totalGuides] = await this.prismaService.$transaction([
+        this.prismaService.city.count({
+          where: {
+            deleted: false,
+            guides: {
+              some: {
+                deleted: false,
+              },
+            },
+          },
+        }),
+        this.prismaService.country.count({
+          where: {
+            deleted: false,
+            guides: {
+              some: {
+                deleted: false,
+              },
+            },
+          },
+        }),
+        this.prismaService.guide.count({
+          where: {
+            deleted: false,
+          },
+        }),
+      ])
+
+      return {
+        totalUsers: 0,
+        totalCities: totalCities,
+        totalGuides: totalGuides,
+        totalCountries: totalCountries,
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        this.logger.error(`Failed to get total stats: ${error.message}`)
+        throw new Error("Failed to get total stats")
+      }
+
+      this.logger.error("Unexpected error getting total stats")
+      throw new Error("Unexpected error getting total stats")
     }
   }
 }
